e6f99d8b09aeddd51ce2e8c001886075ad13924f
Andreas Kochmann <andreaskochmann85@gmail.com>
2019-05-26 10:15:59 +0200

    extracted searchField from app.js to searchbar.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React , { Component } from 'react';
 import { connect } from 'react-redux';
-import { createNumber, giveHint } from "../actions";
+import { createNumber } from "../actions";
 
 import SearchBar from './SearchBar';
 
@@ -9,34 +9,12 @@ class App extends Component {
         super(props);
 
         this.props.createNumber();
-
-        this.state = {      value : '',
-                            guessed : false
-        };
     }
 
     render(){
-    const compareNum = (e) => {
-        e.preventDefault();
-        if (this.state.value === this.props.rndNumber) {
-            this.props.giveHint(this.state.value, this.props.rndNumber);
-            this.setState({ guessed : true });
-        }else{
-            this.props.giveHint(this.state.value, this.props.rndNumber);
-            this.setState({ value : ''});
-        }
-    };
 
     return (
         <div>
-            <form onSubmit={compareNum}>
-                <label>Guess the number</label>
-                <br />
-                <input
-                    disabled={this.state.guessed}
-                    value={this.state.value}
-                    onChange={e => this.setState({ value: parseInt(e.target.value) })} />
-            </form>
             <SearchBar  />
             {this.props.hint}
         </div>
@@ -50,7 +28,4 @@ const mapStateToProps = (state) => {
     }
 };
 
-export default connect(mapStateToProps, {
-                                            createNumber,
-                                            giveHint
-})(App)
+export default connect(mapStateToProps, { createNumber })(App)
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { giveHint } from "../actions";
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {      value : '',
+                            guessed : false
+        };
+    }
+
     render() {
+
+        const compareNum = (e) => {
+            e.preventDefault();
+            if (this.state.value === this.props.rndNumber) {
+                this.props.giveHint(this.state.value, this.props.rndNumber);
+                this.setState({ guessed : true });
+            }else{
+                this.props.giveHint(this.state.value, this.props.rndNumber);
+                this.setState({ value : ''});
+            }
+        };
+
         return (
-            <div>Search Bar</div>
+            <div>
+                <form onSubmit={compareNum}>
+                    <label>Guess the number</label>
+                    <br />
+                    <input
+                        disabled={this.state.guessed}
+                        value={this.state.value}
+                        onChange={e => this.setState({ value: parseInt(e.target.value) })} />
+                </form>
+            </div>
         )
     }
 }
 
-export default SearchBar
+const mapStateToProps = (state) => {
+    return {
+        rndNumber : state.randomNumber,
+    }
+};
+
+export default connect(mapStateToProps, { giveHint }) (SearchBar)
