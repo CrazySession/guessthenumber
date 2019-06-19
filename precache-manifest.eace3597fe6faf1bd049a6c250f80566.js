d6288d0c10637b29e4fffc3f2b66628bee2f9f89
Andreas Kochmann <andreaskochmann85@gmail.com>
2019-06-19 10:36:16 +0200

    Updates

---
+++ precache-manifest.eace3597fe6faf1bd049a6c250f80566.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0ccbeb839fca87f1addee29c741867",
+    "url": "/guessthenumber/index.html"
+  },
+  {
+    "revision": "64222f8b3c9837bd60ea",
+    "url": "/guessthenumber/static/css/main.ca54a786.chunk.css"
+  },
+  {
+    "revision": "d4cc3537a75027792536",
+    "url": "/guessthenumber/static/js/2.98b185bb.chunk.js"
+  },
+  {
+    "revision": "64222f8b3c9837bd60ea",
+    "url": "/guessthenumber/static/js/main.5e8fa052.chunk.js"
+  },
+  {
+    "revision": "4c57b8b07042ed89e4b7",
+    "url": "/guessthenumber/static/js/runtime~main.7cb329bd.js"
+  }
+]);
